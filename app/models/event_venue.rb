59c69b74c66fb5e4fe6b6dc6f3e37af49fc9a0f8
resnakk <masanchez3@miuandes.cl>
2019-04-03 12:06:23 -0300

    Methods

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,2 +1,6 @@
 class EventVenue < ApplicationRecord
+	#validates :name, lenght: {minimum: 20}
+	#validates :capacity, numericality: {greater_than: 0}
+	
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+
+
+
 class User < ApplicationRecord
   has_many :orders
+  validates :password, length: {in: 8..12}
+  validates :phone, length: {in: 9..12}, numericality: true
+  validtes :validate_each 
+
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
 end
