010cc4f3c345bc012ad4dd77cf2c66fdc8391179
resnakk <masanchez3@miuandes.cl>
2019-04-03 15:41:10 -0300

    email

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
-  validate :later_than_current_creation
-  validates :event_venue, uniqueness: {scope: :start_date, message: "You can't have two events at the same venue at the same date" }
+  validates :start_date, presence: true
+  validate :before_than_current_creation
+  validates :event_venue, presence: true, uniqueness: {scope: :start_date,
+    message: "You can't have two events at the same venue at the same date" }
   
-  def later_than_current_creation
+  def before_than_current_creation
   	if start_date < Date.today
   		errors.add(:current_creation, "start date can't be before current date")
   	end
   end
 
-  #def two_events_in_with_the_same_venue
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,5 @@ class User < ApplicationRecord
   has_many :orders
   validates :password, length: {in: 8..12}
   validates :phone, length: {in: 9..12}, numericality: true
-  validtes :validate_each 
-
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
+  validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Invalid email"} 
 end
