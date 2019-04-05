90bc78ee96fe9077032257fe00358345044ab304
Tomas <tmgarcia@miuandes.cl>
2019-04-04 23:48:14 -0300

    esta papipurri

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
   before_validation :ticket_before_start_date
-
+  before_validation :bought_after_start_date
   private
   	def ticket_before_start_date
   		used_ticket = TicketType.find(ticket_type.id)
@@ -11,14 +11,16 @@ class Ticket < ApplicationRecord
   			errors.add(:current_creation, "Can't create a ticket for an old event")
   			return false
   		end
+  	end
 
   	def bought_after_start_date
   		used_ticket = TicketType.find(ticket_type.id)
-  		used_order = Order.find(ticket_order.id)
+  		used_order = Order.find(order.id)
   		used_event = Event.find(used_ticket.event_id)
   		
-  		if 
-
-
+  		if used_order.created_at > used_event.start_date
+  			errors.add(:current_creation, "Can't bought a ticket for an old event")
+  			return false
+  		end
   	end
 end
